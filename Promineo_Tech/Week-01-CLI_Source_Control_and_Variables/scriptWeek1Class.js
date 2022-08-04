55d05215d5069ead6db13adf1271ca5f7cdde0d8
markmccreery <madzmark.mm@gmail.com>
2022-08-04 08:28:04 -0700

    hopefully adds directories

---
+++ Promineo_Tech/Week-01-CLI_Source_Control_and_Variables/scriptWeek1Class.js
@@ -0,0 +1,16 @@
+var clicks = 0;
+
+var strengthOfClick = 1;
+
+clicks = clicks + strengthOfClick;
+ console.log("clicks" + clicks);
+ 
+ clicks = clicks + strengthOfClick
+ console.log("clicks:" + clicks);
+
+ clicks = clicks + strengthOfClick
+ console.log("clicks:" + clicks);
+ console.log(clicks + clicks) //33
+
+ // future code that senses total clicks
+ strengthOfClick = strengthOfClick * 2;
